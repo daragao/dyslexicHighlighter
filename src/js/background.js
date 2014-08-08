df24ab55e0569ac371f7c38419d0e2fc1374d3d7
Duarte Aragao <duarte.aragao@axomic.com>
2014-08-08 16:33:30 +0100

    small changes

--- src/js/background.js
+++ src/js/background.js
@@ -4,10 +4,10 @@ var toggleScript = (function() {
         var scriptFile;
         if(isScriptOn){
         //chrome.browserAction.setIcon({path:"inactive.png"});
-        chrome.tabs.executeScript(null, {file: "scriptOff.js"})
+            chrome.tabs.executeScript(null, {file: "src/js/scriptOff.js"})
         } else {
         //chrome.browserAction.setIcon({path:"icon.png"});
-        chrome.tabs.executeScript(null, {file: "scriptOn.js"})
+        chrome.tabs.executeScript(null, {file: "src/js/scriptOn.js"})
         }
         isScriptOn = !isScriptOn;
         };
--- src/js/scriptAux.js
+++ src/js/scriptAux.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var wordsClass = 'word';
 var wordHighliterId = 'wordHighlighter';
 var div = document.createElement( 'div' );
@@ -9,8 +11,8 @@ function getMouseXY(e) {
     var tempX = e.clientX;
     var tempY = e.clientY;
     // catch possible negative values in NS4
-    if (tempX < 0){tempX = 0}
-    if (tempY < 0){tempY = 0}
+    if (tempX < 0){tempX = 0;}
+    if (tempY < 0){tempY = 0;}
     // show the position values in the form named Show
     // in the text fields named MouseX and MouseY
     if(isFreeline) {
@@ -19,17 +21,16 @@ function getMouseXY(e) {
         var  elementMouseIsOver = document.elementFromPoint(tempX,tempY);
         var wordCoord = highlightWords(elementMouseIsOver);
         var hasChanged = moveWordHighlighter(wordCoord);
-        if(hasChanged)
+        if(hasChanged) {
             console.log($(elementMouseIsOver).text());
+        }
     }
-    return true
+    return true;
 }
 
 function moveWordHighlighter(wordCoord) {
     var $wordHighliterElem = $('#'+wordHighliterId);
-    if(!_.isEqual(wordCoord,$wordHighliterElem.data('wordCoord'))
-            && wordCoord &&
-            $.isPlainObject(wordCoord)){
+    if( !_.isEqual(wordCoord,$wordHighliterElem.data('wordCoord')) && wordCoord && $.isPlainObject(wordCoord)) {
         $wordHighliterElem.data('wordCoord',_.clone(wordCoord));
         var options = {queue:false};
         var sideSpace = 5;
@@ -52,27 +53,27 @@ function unwrapWords() {
 function highlightWords(element) {
     var classStr = wordsClass;
     var $element = $(element);
-        var $dyslexiaElems = $element.find('.'+classStr).andSelf().
+    var $dyslexiaElems = $element.find('.'+classStr).andSelf().
         filter('.'+classStr);
     if(!$dyslexiaElems.length) {
         var contents = $element.contents();
         var textContents = contents.filter(
-                function(){
-                return this.nodeType !== 1
-                });
+            function(){
+            return this.nodeType !== 1
+        });
         textContents.replaceWith(function() {
-                return (this.nodeValue || "").replace(
-                    /(\w+)/g,
-                    function(match) {
+            return (this.nodeValue || "").replace(
+                /(\w+)/g,
+                function(match) {
                     return "<span class=\""+classStr+"\">" + match + "</span>";
-                    });
                 });
+        });
     } else if($dyslexiaElems.length===1) {
         var wordCoord = {
-top: $dyslexiaElems.offset().top,
-     left: $dyslexiaElems.offset().left,
-     height: $dyslexiaElems.height(),
-     width: $dyslexiaElems.width()
+            top: $dyslexiaElems.offset().top,
+            left: $dyslexiaElems.offset().left,
+            height: $dyslexiaElems.height(),
+            width: $dyslexiaElems.width()
         };
         return wordCoord;
     }
